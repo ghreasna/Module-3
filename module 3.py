a1807c186329104aaae6627cf6822639acb36e87
ghreasna <sonighreasna@gmail.com>
2025-07-24 18:30:23 +0530

    Add files via upload

---
+++ module 3.py
@@ -0,0 +1,38 @@
+@ -0,0 +1,37 @@
+# module 2 .py
+# <!-- in this we use conditional statement -->
+
+
+num = int(input("enter the number: "))
+if num % 2 == 0:
+    print("num is even number")
+else :
+    print("num is odd number")
+
+# if we print here num = 13 , than it print("number is odd")
+# if we enter num = 8 , than it print (number is even)
+
+
+# program in which we add 
+
+n = int(input("enter a number: "))
+i = 1
+sum = 0
+while (i<=n):
+    sum += i
+    i+1 == 1
+    print("addition of number is", sum)
+
+
+
+n = int(input("Enter a number: "))
+i = 1
+sum = 0
+
+while i <= n:
+    sum += i
+    i += 1  
+print("Addition of numbers is", sum)
+
+
+# sum of 1-50 is ; 1275
